a5dc450260cf5fe7ab1d060fcd3359257b436ab8
renatatoth <renata.anna.toth@gmail.com>
2025-03-19 12:39:54 +0100

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyCrtvPhXl-EQoBFUKTSW95TN8VGPWLUeFs',
+        authDomain: 'fws-test-deecb.firebaseapp.com',
+        projectId: 'fws-test-deecb',
+        storageBucket: 'fws-test-deecb.firebasestorage.app',
+        messagingSenderId: '917647456665',
+        appId: '1:917647456665:web:63720c2729967c65d07b08',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
